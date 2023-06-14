47b276b6bf9b59bffbd14dd97f97e02d1101dfc8
Fahid21 <mdfahidhasn871@gmail.com>
2023-06-14 15:53:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,207 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const verifyJWT = (req,res,next) =>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error:true, message:'unauthorized access'});
+    }
+
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+        if(err){
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ist6ay7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const usersCollection = client.db("bistroDb").collection("users");
+        const menuCollection = client.db("bistroDb").collection("menuCollection");
+        const cartCollection = client.db("bistroDb").collection("cartCollection");
+        const reviewCollection = client.db("bistroDb").collection("reviews ");
+
+
+        app.post('/jwt', (req,res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+
+            res.send({token})
+        })
+
+// use verifyJwt before using verifyAdmin
+        const verifyAdmin = async (req,res,next) =>{
+            const email = req.decoded.email;
+            const query = {email:email}
+            const user = await usersCollection.findOne(query);
+            if(user?.role !== 'admin'){
+                return res.status(403).send({error: true, message: 'forbidden message'});
+            }
+        next();}
+
+        app.get('/users',verifyJWT,verifyAdmin, async (req,res) =>{
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+        // users related apis
+        app.post('/users', async (req,res) =>{
+            const user = req.body;
+            
+            const query = {email: user.email}
+            const existingUSer = await usersCollection.findOne(query);
+            if (existingUSer){
+                return res.send({message:'user already exists'})
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+          
+        app.get('/users/admin/:email',verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const decodedEmail = req.decoded.email;
+            if ( email !== decodedEmail){
+             return   res.send({admin: false})
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            res.send ({admin: user?.role === 'admin'})
+           
+
+         } )
+        
+
+        app.patch('/users/admin/:id', async (req,res) =>{
+            const id = req.params.id;
+            const filter ={_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {role: 'admin'},
+            }
+
+            const result = await usersCollection.updateOne(filter,updateDoc);
+            res.send(result);
+        })
+
+        // menu related apis
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+
+        })
+
+                app.post('/menu',verifyJWT,verifyAdmin, async (req, res) => {
+                    const newItem = req.body;
+                    const result = await menuCollection.insertOne(newItem)
+                    res.send(result)
+                })
+
+        app.delete('/menu/:id',verifyJWT,verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            console.log(result);
+
+            res.send(result);
+        })
+
+
+        // cart collection apis
+        app.get('/carts',verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+                return res.status(403).send({ error: true, message: 'porviden access' })
+
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+        // cart collection
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        app.post("/create-payment-intent",verifyJWT, async (req, res) => {
+            const { price } = req.body;
+
+            const amount = price*100;
+            console.log(price,amount);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                automatic_payment_methods: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('boss is exhausted')
+})
+
+app.listen(port, () => {
+    console.log(`Bistro boss is sitting on port ${port}`);
+})
